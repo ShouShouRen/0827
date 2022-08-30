63e9a391809527fff2cde03f81e40791d45bdded
Peter Chen <peterchen2017125@gmail.com>
2022-08-30 13:32:14 +0800

    20220830

--- js/index.js
+++ js/index.js
@@ -10,16 +10,7 @@ $(window).scroll(function () {
 })
 
 
-var swiper = new Swiper(".mySwiper", {
-  direction: "vertical",
-  slidesPerView: 1,
-  spaceBetween: 30,
-  mousewheel: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+
 $('.nav-link,a').click(function () {
   var t = $(this).attr('href');
   var st = $(t).offset().top - 100;
