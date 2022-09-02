1a9e3855ef26d093fe6a6cf2b2ee14835259be47
Peter Chen <peterchen2017125@gmail.com>
2022-09-02 13:03:28 +0800

    20220902

--- js/all.js
+++ js/all.js
@@ -1,25 +1,24 @@
-$(window).scroll(function () {
-    if ($(window).width() < 768) {
-      $("#navbarNav a").click(function () {
-        $("#navbarNav").collapse('hide');
-      });
-      $(window).scroll(function () {
-        $("#navbarNav").collapse('hide');
-      });
-    }
-  })
-  
-  
-  
-  $('.nav-link,a').click(function () {
-    var t = $(this).attr('href');
-    var st = $(t).offset().top - 100;
-    $('html,body').animate({
-      scrollTop: st
-    }, 500);
-  });
-  
-  
-  $('.submit').click(function() {
-    $('input').val('');
-  })
+$(document).ready(function () {
+  $(window).scroll(function () {
+      if ($(window).width() < 768) {
+        $("#navbarNav a").click(function () {
+          $("#navbarNav").collapse('hide');
+        });
+        $(window).scroll(function () {
+          $("#navbarNav").collapse('hide');
+        });
+      }
+    })
+    
+    $('.nav-link,a').click(function () {
+      var t = $(this).attr('href');
+      var st = $(t).offset().top - 100;
+      $('html,body').animate({
+        scrollTop: st
+      }, 500);
+    });
+    
+    $('.submit').click(function() {
+      $('input').val('');
+    })
+});
