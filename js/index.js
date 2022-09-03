9d11bcaceba1e5f7a04025547805f580efe0bf62
Peter Chen <peterchen2017125@gmail.com>
2022-09-03 09:05:37 +0800

    20220903

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
 Vue.createApp({
   data() {
     return {
+      ness: [{
+          title: '美味窯烤放山雞冷凍包新上市',
+          img: './images/new-a.jpg'
+        },
+        {
+          title: '當日現擠牛奶新上市',
+          img: './images/new-b.jpg'
+        },
+        {
+          title: '本農場蔬食打9折',
+          img: './images/new-c.jpg'
+        },
+      ],
       products: [{
           title: '美味烤雞',
           content: '飼養在深山裡的放山雞，本農場將雞高溫烘烤製成快速又方便的調理包，加熱僅可使用',
@@ -45,7 +58,17 @@ Vue.createApp({
           price: 40,
           amountShow: 0,
         },
-      ]
+      ],
+      keys: [{
+        title: '自然',
+        content: '厚德自然農場全區皆不使用農藥及除草劑，以避免有任何化學物質殘留，吃進身體影響健康。因此在種植的過程中我們飼養放山雞在苦茶樹下面，讓雞來吃蟲吃雜草，雞糞再變成樹木的肥料',
+      }, {
+        title: '安心',
+        content: '放山雞也因為整天在外運動，抵抗力比養在籠子裡的雞還要好，因此根本不需要打針用藥，如此的循環才是我們理想中與自然和諧相處的方式。',
+      }, {
+        title: '健康',
+        content: '本農場注重養身健康，以無毒無害為原則不管是在種植養殖都層層遵守我們的原則，希望消費者吃得健康又安心，也同時保護到大自然環境，可說是一舉兩得。'
+      }]
     }
   },
   methods: {
@@ -59,11 +82,11 @@ Vue.createApp({
     },
   },
   computed: {
-    sub(){
+    sub() {
       return this.products.map(data => data.price * data.amountShow)
     },
     total() {
-      return this.sub.reduce((a,b )=> a+b)
+      return this.sub.reduce((a, b) => a + b)
     },
   }
 
