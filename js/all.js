fb55490b3eebc10e54c6fc3935b205025edba847
Peter Chen <peterchen2017125@gmail.com>
2022-09-05 10:48:27 +0800

    20220905

--- js/all.js
+++ js/all.js
@@ -1,29 +1,29 @@
 $(document).ready(function () {
   $(window).scroll(function () {
-      if ($(window).width() < 768) {
-        $("#navbarNav a").click(function () {
-          $("#navbarNav").collapse('hide');
-        });
-        $(window).scroll(function () {
-          $("#navbarNav").collapse('hide');
-        });
-      }
-    })
-    
-    $('.nav-link,a').click(function () {
-      var t = $(this).attr('href');
-      var st = $(t).offset().top - 100;
-      $('html,body').animate({
-        scrollTop: st
-      }, 500);
-    });
-    
-    $('.submit').click(function() {
-      $('#contact input, #contact textarea').val('');
-    })
+    if ($(window).width() < 768) {
+      $("#navbarNav a").click(function () {
+        $("#navbarNav").collapse('hide');
+      });
+      $(window).scroll(function () {
+        $("#navbarNav").collapse('hide');
+      });
+    }
+  })
 
-    $('.modal-footer .clearcart').click(function() {
-      alert("感謝您的購買本農場會儘速送達！！！")
-      location.reload();
-    })
+  $('.nav-link,a').click(function () {
+    var t = $(this).attr('href');
+    var st = $(t).offset().top - 100;
+    $('html,body').animate({
+      scrollTop: st
+    }, 500);
+  });
+
+  $('.submit').click(function () {
+    $('#contact input, #contact textarea').val('');
+  })
+
+  $('.modal-footer .clearcart').click(function () {
+    alert("感謝您的購買本農場會儘速送達！！！")
+    location.reload();
+  })
 });
