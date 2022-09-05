1ac07743f1cceaba6264c24bed6f76e84da88904
Peter Chen <peterchen2017125@gmail.com>
2022-09-05 16:52:57 +0800

    20220905

--- js/all.js
+++ js/all.js
@@ -26,4 +26,18 @@ $(document).ready(function () {
     alert("感謝您的購買本農場會儘速送達！！！")
     location.reload();
   })
+
+  $('#gotop').click(function () {
+    $('html ,body').animate({
+      scrollTop: 0
+    })
+    return false;
+  })
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 400) {
+      $('#gotop').fadeIn();
+    } else {
+      $('#gotop').fadeOut();
+    }
+  })
 });
